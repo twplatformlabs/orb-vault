dddc21aa181b17f4f0de3a241b6d4fed7e6ab593
olivercodes <bryan@olivercodes.com>
2025-06-03 15:28:44 -0400

    update teller install cmd

--- src/scripts/install_teller.sh
+++ src/scripts/install_teller.sh
@@ -3,5 +3,7 @@ set -e pipefail
 
 echo "TELLER_VERSION $TELLER_VERSION"
 
-curl -sSfLo teller.zip "https://github.com/tellerops/teller/releases/download/v${TELLER_VERSION}/teller_${TELLER_VERSION}_Linux_x86_64.tar.gz"
-sudo unzip -od "/usr/local/bin" teller.zip && rm teller.zip
+curl -sSLO "https://github.com/tellerops/teller/releases/download/v${TELLER_VERSION}/teller_${TELLER_VERSION}_Linux_x86_64.tar.gz"
+sudo tar -xvf teller_1.5.6_Linux_x86_64.tar.gz
+sudo mv teller /usr/local/bin/.
+rm -rf teller
