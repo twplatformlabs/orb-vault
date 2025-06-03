8b6f8201581a90e47b689ce58d1b85f3a2b9bed2
olivercodes <bryan@olivercodes.com>
2025-06-03 15:20:29 -0400

    teller_version, not vault

--- src/scripts/install_teller.sh
+++ src/scripts/install_teller.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e pipefail
 
-echo "TELLER_VERSION $VAULT_VERSION"
+echo "TELLER_VERSION $TELLER_VERSION"
 
 curl -sSfLo teller.zip "https://github.com/tellerops/teller/releases/download/v${TELLER_VERSION}/teller_${TELLER_VERSION}_Linux_x86_64.tar.gz"
 sudo unzip -od "/usr/local/bin" teller.zip && rm teller.zip
