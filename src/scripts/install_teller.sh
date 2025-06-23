f9485a58397b3b844d682a33eeb680921f397c1d
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-06-23 16:52:05 -0500

    [nc] minor cleanup
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_teller.sh
+++ src/scripts/install_teller.sh
@@ -3,7 +3,7 @@ set -e pipefail
 
 echo "TELLER_VERSION $TELLER_VERSION"
 
-curl -sSLO "https://github.com/tellerops/teller/releases/download/v${TELLER_VERSION}/teller_${TELLER_VERSION}_Linux_x86_64.tar.gz"
-sudo tar -xvf teller_1.5.6_Linux_x86_64.tar.gz
-sudo mv teller /usr/local/bin/.
-rm -rf teller
+curl -SLO "https://github.com/tellerops/teller/releases/download/v${TELLER_VERSION}/teller_${TELLER_VERSION}_Linux_x86_64.tar.gz"
+sudo tar -xvf "teller_${TELLER_VERSION}_Linux_x86_64.tar.gz" teller
+sudo mv teller /usr/local/bin/teller
+rm "teller_${TELLER_VERSION}_Linux_x86_64.tar.gz"
--- src/scripts/install_vault.sh
+++ src/scripts/install_vault.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e pipefail
 
-
 echo "VAULT_VERSION $VAULT_VERSION"
 
-# install linux/intel arch
 curl -sSfLo vault.zip "https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip"
 sudo unzip -od "/usr/local/bin" vault.zip && rm vault.zip
