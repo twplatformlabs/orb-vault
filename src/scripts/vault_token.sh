38a68a1e809c212180fa4897923eed531ca27053
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-06-23 22:20:22 -0500

    [nc] add write-to-file command
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/vault_token.sh
+++ src/scripts/vault_token.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eo pipefail
 
-VAULT_TOKEN=$(vault write -namespace="$VAULT_NAMESPACE" -format=json auth/approle/login \
-  role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID" | jq -r .auth.client_token)
+VAULT_TOKEN=$(vault write -namespace="$VAULT_NAMESPACE" -format=json \
+                           auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID" | jq -r .auth.client_token)
 echo "export VAULT_TOKEN=$VAULT_TOKEN" >> "$BASH_ENV"
