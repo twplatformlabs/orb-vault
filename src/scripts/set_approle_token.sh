cfa02b6b71d8a0a9f7d9f7168a5bd509b61cbde0
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-06-25 16:38:48 -0500

    [nc] add approle named auth command
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/set_approle_token.sh
+++ src/scripts/set_approle_token.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-echo "VAULT_ADDR ${VAULT_ADDR}"
-echo "VAULT_NAMESPACE ${VAULT_NAMESPACE}"
-echo "VAULT_ROLE_ID ${VAULT_ROLE_ID:0:7}"
-echo "VAULT_SECRET_ID ${VAULT_SECRET_ID:0:7}"
-
+# assumes the following env vars are set in the circleci context
 VAULT_TOKEN=$(vault write -namespace="$VAULT_NAMESPACE" -format=json \
                            auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID" | jq -r .auth.client_token)
 echo "export VAULT_TOKEN=$VAULT_TOKEN" >> "$BASH_ENV"
